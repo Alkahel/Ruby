13fd4faf904001b3a6acc2277b67c4777b0af52f
Alkhael <68680243+Alkahel@users.noreply.github.com>
2024-06-04 22:40:19 -0500

    Add files via upload

---
+++ JS/carrousel.js
@@ -0,0 +1,29 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-image');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+setInterval(nextSlide, 3000);
+
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
+
